302e3ca670f8f602bb05c9c20bd11da4c066dfe0
Sukhchain Dhallu <sukhchaindhallu@gmail.com>
2025-10-23 18:49:14 -0400

    Import pandas and load dataset from Excel file
    Added code to import pandas and load dataset from Excel.

--- preprocessing.py
+++ preprocessing.py
@@ -1,1 +1,10 @@
+import pandas as pd
+
+file_path = r'Dataset/PulseBat Dataset.xlsx'
+
+# Load the dataset from the specified pulsebat datasheet
+df_dataset = pd.read_excel(file_path, sheet_name='Data')
+
+
+
 
