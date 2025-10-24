7371dcd0aaf3eaf199e1147014a41e7a026c2433
IsaiahGodinez <isaiahdgodinez@gmail.com>
2025-10-23 20:49:45 -0400

    create plot

--- linearReg.py
+++ linearReg.py
@@ -2,5 +2,16 @@ import numpy as np
 import pandas as pd
 from sklearn.linear_model import LinearRegression
 import matplotlib.pyplot as plt
-import preprocessing
+import preprocessing as pp
 
+x_data = pp.dataset.drop(columns=['SOH'])
+x_data = np.array(x_data)
+
+y_data = pp.dataset['SOH']
+y_data = np.array(y_data)
+
+plt.scatter(x_data, y_data)
+
+plt.xlabel("Data")
+plt.ylabel("SOH")
+plt.title("Linear Regression")
--- preprocessing.py
+++ preprocessing.py
@@ -1,9 +1,9 @@
 import pandas as pd
 
-file_path = r'Dataset/PulseBat Dataset.xlsx'
+file_path = r'PulseBat Dataset.xlsx'
 
 # Load the dataset from the specified pulsebat datasheet
-dataset = pd.read_excel(file_path, sheet_name='Data')
+dataset = pd.read_excel(file_path, sheet_name='SOC ALL')
 
 # Drop unnecessary columns
 dataset.drop(columns=['Mat', 'No.', 'ID'], inplace=True)
