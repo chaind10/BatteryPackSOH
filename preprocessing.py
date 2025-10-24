c3766211f5554932195293fd0ff4b55ba68d8c27
Sukhchain Dhallu <sukhchaindhallu@gmail.com>
2025-10-23 20:21:16 -0400

    changing dataset variable name in preprocessing.py

--- preprocessing.py
+++ preprocessing.py
@@ -3,22 +3,22 @@ import pandas as pd
 file_path = r'Dataset/PulseBat Dataset.xlsx'
 
 # Load the dataset from the specified pulsebat datasheet
-df_dataset = pd.read_excel(file_path, sheet_name='Data')
+dataset = pd.read_excel(file_path, sheet_name='Data')
 
 # Drop unnecessary columns
-df_dataset.drop(columns=['Mat', 'No.', 'ID'], inplace=True)
+dataset.drop(columns=['Mat', 'No.', 'ID'], inplace=True)
 
 # Remove missing values
-df_dataset.dropna(inplace=True)
+dataset.dropna(inplace=True)
 
 # Ensure all numeric columns are numeric
-df_dataset = df_dataset.apply(pd.to_numeric, errors='ignore')
+dataset = dataset.apply(pd.to_numeric, errors='ignore')
 
 # Sort by 'Q' (charge quantity)
-sortedByQ = df_dataset.sort_values(by='Q')
+sortedByQ = dataset.sort_values(by='Q')
 
 # Sort by 'SOC' (state of charge)
-sortedBySOC = df_dataset.sort_values(by='SOC')
+sortedBySOC = dataset.sort_values(by='SOC')
 
 #Saving the sorted datasets for later reference
 sortedByQ.to_csv('Dataset/sorted_by_Q.csv', index=False)
@@ -26,5 +26,4 @@ sortedBySOC.to_csv('Dataset/sorted_by_SOC.csv', index=False)
 
 
 u_cols = [f'U{i}' for i in range(1, 22)] #creating a list of each cell voltage column (1 - 21)
-df_dataset['AvgCellVoltage'] = df_dataset[u_cols].mean(axis=1) #calculating average cell voltage for each row
-
+dataset['AvgCellVoltage'] = dataset[u_cols].mean(axis=1) #calculating average cell voltage for each row
