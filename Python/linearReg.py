59305e53ab3e59e0c6cd6411dc2051acdddfc2e1
chaind10 <sukhchaindhallu@gmail.com>
2025-11-17 20:25:40 -0500

    New Commit

---
+++ Python/linearReg.py
@@ -0,0 +1,41 @@
+import numpy as np
+import pandas as pd
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import r2_score, mean_squared_error, mean_absolute_error
+import matplotlib.pyplot as plt
+import preprocessing as pp 
+
+#extracting columns (u1-u21) for cell voltages and 'SOH' for state of health
+u_cols = [f'U{i}' for i in range(1, 22)]
+x_data = pp.dataset[u_cols + ['SOE']].values
+y_data = pp.dataset['SOH'].values
+
+#x_train and y_train are arrays used to train the model
+#x_test and y_test are arrays used to test the model
+#20% of the data for testing and 80% for training
+X_train, X_test, y_train, y_test = train_test_split(
+    x_data, y_data, test_size=0.1, random_state=1
+)
+
+#Creating and training the linear regression model
+regModel = LinearRegression()
+regModel.fit(X_train, y_train)
+
+#making predictions using the test data values
+predictions = regModel.predict(X_test)
+
+#testing the model by comparing predicted values to actual values
+r2 = r2_score(y_test, predictions)
+MSE = mean_squared_error(y_test, predictions)
+MAE = mean_absolute_error(y_test, predictions)
+
+#stating the results of the model
+print(f"R^2 Score: {r2}")
+print(f"Mean Squared Error: {MSE}")
+print(f"Mean Absolute Error: {MAE}")
+
+#0.6 threshold
+threshold = input("What is the threshold:") or 0.6
+if (predictions >= threshold):
+    print("The battery is healthy.")
---
+++ Python/preprocessing.py
@@ -0,0 +1,22 @@
+import pandas as pd
+
+file_path = r'Dataset/PulseBat Dataset.xlsx'
+
+# Load the dataset from the specified pulsebat datasheet
+dataset = pd.read_excel(file_path, sheet_name='SOC ALL')
+
+# Drop unnecessary columns
+dataset.drop(columns=['Mat', 'No.', 'ID', 'Qn', 'SOC', 'Pt'], inplace=True)
+
+# Remove missing values
+dataset.dropna(inplace=True)
+
+# Sort by 'Q' (charge quantity)
+#sortedByQ = dataset.sort_values(by='Q')
+
+# Sort by 'SOC' (state of charge)
+#sortedBySOC = dataset.sort_values(by='SOC')
+
+#Saving the sorted datasets for later reference
+#sortedByQ.to_csv('Dataset/sorted_by_Q.csv', index=False)
+#sortedBySOC.to_csv('Dataset/sorted_by_SOC.csv', index=False)
